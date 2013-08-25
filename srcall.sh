cd839cf7e6dc3f78200f5b3d00fd72d0c0799c33
Antonio Dell'Elce <adellelce@hotmail.com>
2013-08-25 14:32:19 +0200

    added test script that downloads all profiles

---
+++ srcall.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# File:         srcall.sh
+# Created:      1703 100813
+# Description:  description for srcall.sh
+#
+
+### FUNCTIONS ###
+
+srcget()
+{
+ typeset srcget="$srcgetDir/srcget.sh"
+
+ [ ! -f "$srcget" ] && return 1
+ [ ! -x "$srcget" ] && return 2
+
+ $srcget $*
+}
+
+### ENV ###
+
+
+srcgetDir="$(dirname $0)"
+
+### MAIN ###
+
+[ ! -d "$srcgetDir/profiles" ] &&
+{
+  echo "Can't find profiles directory!: $srcgetDir/profiles"
+  exit 2
+}
+
+for x in $srcgetDir/profiles/*;
+do
+ b=$(basename $x);
+ p=${b%.profile};
+
+
+ echo "========== $p";
+ printf "Checking..."
+ srcget $p > /dev/null
+ rc="$?"
+
+ # wget appears to return 1 on success.......(!?)
+ [ $rc -eq 0 -o $rc -eq 1 ] && { echo "downloaded"; continue; } 
+ [ $rc -eq 2 ] && { echo "already latest"; continue; }
+
+ echo "error: $rc"
+
+done
+
+
+### EOF ###
