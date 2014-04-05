622bc00fd104477ed98f875794de0585f6a2e0fa
Antonio Dell'Elce <adellelce@hotmail.com>
2014-04-05 21:09:29 +0200

    fix: correct variable name

--- srcget.sh
+++ srcget.sh
@@ -314,7 +314,7 @@ srcall()
    # profile is loaded twice, and this is not a good thing, but for now we can live with it....
    load_profile "$p"
    [ "$bulkenabled" == "no" -o "$bulkenabled" == "n" ] && continue # ignore profiles not enabled for bulk (srcall): profiles in development
-   [ ! -z "$basename" ] && { b="$basename"; } # override if set in profile!
+   [ ! -z "$basename" ] && { p="$basename"; } # override if set in profile!
 
    # always run main_single silently
    SILENT=1 main_single $p
